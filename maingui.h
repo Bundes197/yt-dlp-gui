1e0a01f80de35c3309f59d6c8eb510ad913e5757
Filip Funda <filipfunda@gmail.com>
2025-12-11 17:49:30 +0100

    feat: blank qt project

---
+++ maingui.h
@@ -0,0 +1,23 @@
+#ifndef MAINGUI_H
+#define MAINGUI_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainGUI;
+}
+QT_END_NAMESPACE
+
+class MainGUI : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainGUI(QWidget *parent = nullptr);
+    ~MainGUI();
+
+private:
+    Ui::MainGUI *ui;
+};
+#endif // MAINGUI_H
